d3b1f6d3fc436dd51f1cc9c873a908a8a7f41b09
Valen Blanco <valentin_gomez1@hotmail.com>
2014-06-16 13:10:07 +0200

    Begin CUP implementation

---
+++ src/pvm/compiler/Main.java
@@ -0,0 +1,15 @@
+package pvm.compiler;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class Main {
+   public static void main(String[] args) throws Exception {
+     Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+	 AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
+	 AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);
+	 asint.parse();
+ }
+}   
+   
