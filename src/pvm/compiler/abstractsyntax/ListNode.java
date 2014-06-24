569b5f8fe229e7559dc6161a9977a72b6fcce755
Valen Blanco <valentin_gomez1@hotmail.com>
2014-06-24 10:24:16 +0200

    Little change in abstract syntax

--- src/pvm/compiler/abstractsyntax/ListNode.java
+++ src/pvm/compiler/abstractsyntax/ListNode.java
@@ -2,13 +2,16 @@ package pvm.compiler.abstractsyntax;
 
 import java.util.List;
 
-public class ListNode<E> extends Node {
+public class ListNode<E extends Node> extends Node {
 	private List<E> list;
 
 	public ListNode(List<E> list) {
 		super();
 
 		this.list = list;
+		
+		for (E elem : list)
+			this.childs.add(elem);
 	}
 
 	public List<E> getList() {
